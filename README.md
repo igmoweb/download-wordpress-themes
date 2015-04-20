adf82a7ea74a63e41137cc5cccb68bdaf033482c
igmoweb <igmoweb@gmail.com>
2015-04-20 12:26:49 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Download WordPress Themes
---
+++ download-themes.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+themes_dir=`pwd`
+
+echo "What repository would you like to get those themes from? wp.com (c)/wp.org (o) ? [Default:wp.org]"
+read REPO
+
+if [[ -z "$REPO" ]]
+then
+	REPO='o'
+fi
+
+if [ $REPO == 'c' ]
+then
+	REPO='c'
+	repo_url='https://wpcom-themes.svn.automattic.com/'
+else
+	REPO='o'
+	repo_url='http://themes.svn.wordpress.org/'
+fi
+
+echo "Insert the themes (one per line) and press ctrl+D"
+MSG=$(cat)
+
+echo "Fetching themes from $repo_url. This could take some time"
+
+for theme in $MSG
+do
+	svn co -q $repo_url$theme'/' $theme
+
+	if [ $REPO == 'o' ]
+	then
+		last_version=`ls $theme | tail -1`
+		for x in `ls $theme`
+		do
+			if [ $last_version != $x ]
+			then
+				rm -r $themes_dir"/"$theme"/"$x
+			fi
+		done
+
+		mv $theme"/"$last_version/*  $theme"/"
+		rm -r $theme"/"$last_version
+	fi
+
+	rm -r $theme"/.svn"
+done
+
+
+
+exit 1
+
